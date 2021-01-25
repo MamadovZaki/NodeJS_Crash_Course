ca977beef327a20dfb865ee18344698b02da205e
MamadovZaki <mamadovzicx@gmail.com>
2021-01-25 20:01:02 +0200

    Example: Implemented a web server
    nodemon: allows us to refresh the web page and see the content that we
    updated without restarting the server.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "/public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(content);
+      }
+    );
+  }
+  if (request.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "/public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(content);
+      }
+    );
+  }
+  if (request.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob",
+        age: 40,
+      },
+      {
+        name: "John",
+        age: 60,
+      },
+    ];
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Sever is running at ${PORT}`));
