c4c56f1e31290d349048017b19e5f5ad1108babd
MamadovZaki <mamadovzicx@gmail.com>
2021-01-25 19:04:44 +0200

    Example: Logging with event emitters

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // you got access to __dirname and __filename variables 
 //console.log(__dirname, __filename);
 
-const Person = require('./person')
+//const Person = require('./person')
 
-const persona = new Person('Zaki', 24 )
+//const persona = new Person('Zaki', 24 )
 
-persona.greeting()
+//persona.greeting()
+
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener:', data));
+
+logger.log('Hello World'); 
