6f7656c77710c91baf0a9a49f74ce6ee0ca8ffd9
MamadovZaki <mamadovzicx@gmail.com>
2021-01-25 19:18:43 +0200

    Moved files to reference

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-// you got access to __dirname and __filename variables 
-//console.log(__dirname, __filename);
-
-//const Person = require('./person')
-
-//const persona = new Person('Zaki', 24 )
-
-//persona.greeting()
-
-
-const Logger = require('./logger');
-
-const logger = new Logger();
-
-logger.on('message', (data) => console.log('Called Listener:', data));
-
-logger.log('Hello World'); 
+const Logger = require("./logger");
